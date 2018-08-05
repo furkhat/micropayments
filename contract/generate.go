7f9dddab7aeafbc0a73d4fbb28a5680f2fe5df62
furkhat <vailodf@gmail.com>
2018-08-05 19:01:47 +0600

    add contracts

---
+++ contract/generate.go
@@ -0,0 +1,4 @@
+package contract
+
+//go:generate abigen --abi abis/psc.abi --pkg contract --type PrivatixServiceContract --out psc.go
+//go:generate abigen --abi abis/ptc.abi --pkg contract --type PrivatixTokenContract --out ptc.go
