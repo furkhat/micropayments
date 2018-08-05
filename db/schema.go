ba8ad9bebec82d11f78ecc089ad290e9fd227d19
furkhat <vailodf@gmail.com>
2018-08-05 18:53:37 +0600

    define account schema

--- db/schema.go
+++ db/schema.go
@@ -1,1 +1,12 @@
 package db
+
+//go:generate reform
+
+// Account is an ethereum account.
+//reform:accounts
+type Account struct {
+	ID         string `reform:"id,pk"`
+	EthAddr    string `reform:"eth_addr"`
+	PublicKey  string `reform:"public_key"`
+	PrivateKey string `reform:"private_key"`
+}
---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Accounts are ethereum accounts.
+CREATE TABLE accounts(
+    id uuid PRIMARY KEY,
+    eth_addr char(40) NOT NULL -- ethereum address
+      CONSTRAINT unique_eth_addr UNIQUE
+    public_key text NOT NULL,
+    private_key text NOT NULL,
+)
