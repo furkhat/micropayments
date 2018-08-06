b9fde5c24915d0f1e68f3cd4da66c1ae5ba99a7a
furkhat <vailodf@gmail.com>
2018-08-06 17:59:22 +0300

    balance fields

--- data/schema.go
+++ data/schema.go
@@ -1,12 +1,35 @@
 package data
 
+import (
+	"encoding/base64"
+	"fmt"
+	"math/big"
+)
+
 //go:generate reform
+
+// B64BigInt is a base64 of big.Int that implements json.Marshaler.
+type B64BigInt string
+
+// MarshalJSON marshals itself.
+func (i B64BigInt) MarshalJSON() ([]byte, error) {
+	buf, err := base64.URLEncoding.DecodeString(string(i))
+	if err != nil {
+		return nil, fmt.Errorf("could not decode base64: %v", err)
+	}
+	v := big.NewInt(0)
+	v.SetBytes(buf)
+	return []byte(v.String()), nil
+}
 
 // Account is an ethereum account.
 //reform:accounts
 type Account struct {
-	ID         string `reform:"id,pk"`
-	EthAddr    string `reform:"eth_addr"`
-	PublicKey  string `reform:"public_key"`
-	PrivateKey string `reform:"private_key"`
+	ID         string    `reform:"id,pk"`
+	EthAddr    string    `reform:"eth_addr"`
+	PublicKey  string    `reform:"public_key"`
+	PrivateKey string    `reform:"private_key"`
+	PTCBalance uint64    `json:"ptcBalance" reform:"ptc_balance"`
+	PSCBalance uint64    `json:"psc_balance" reform:"psc_balance"`
+	EthBalance B64BigInt `json:"ethBalance" reform:"eth_balance"`
 }
--- data/schema.sql
+++ data/schema.sql
@@ -4,5 +4,13 @@ CREATE TABLE accounts(
     eth_addr char(40) NOT NULL -- ethereum address
       CONSTRAINT unique_eth_addr UNIQUE,
     public_key text NOT NULL,
-    private_key text NOT NULL
+    private_key text NOT NULL,
+
+    ptc_balance bigint NOT NULL -- PTC balance
+        CONSTRAINT positive_ptc_balance CHECK (accounts.ptc_balance >= 0),
+
+    psc_balance bigint NOT NULL -- PSC balance
+        CONSTRAINT positive_psc_balance CHECK (accounts.psc_balance >= 0),
+
+    eth_balance char(32) NOT NULL -- ethereum balance up to 99999 ETH in WEI. Ethereum's uint192 in base64 (RFC-4648).
 )
